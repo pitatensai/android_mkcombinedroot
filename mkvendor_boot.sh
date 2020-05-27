67db22a0e4825e72ea078d6ef247239a441bfdff
Bian Jin chen <kenjc.bian@rock-chips.com>
2020-05-27 16:06:38 +0800

    Add vendor image support.
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>

--- mkvendor_boot.sh
+++ mkvendor_boot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 KERNEL_IMAGE=../kernel/arch/arm64/boot/Image
 PRIVATE_MODULE_DIR=./ramdisk/lib/modules
-PRIVATE_LOAD_FILE=./ramdisk_modules.load
+PRIVATE_LOAD_FILE=./res/ramdisk_modules.load
 TEMP_MODULES_PATH=./temp/lib/modules/0.0
 KERNEL_DRIVERS_PATH=../kernel/drivers
 if [ ! -n "$1" ]; then
@@ -13,9 +13,6 @@ fi
 export PATH=$PATH:./bin
 
 echo "==========================================="
-echo -e "\033[33mYou can create ramdisk_modules.load here to customize modules.load\033[0m"
-echo -e "\033[33mUse DTS as $DTB_PATH\033[0m"
-echo "==========================================="
 echo "Preparing temp dirs and use placeholder 0.0..."
 if [ -d temp ]; then
   rm -rf temp
@@ -23,8 +20,11 @@ fi
 if [ -d $PRIVATE_MODULE_DIR ]; then
   rm -rf $PRIVATE_MODULE_DIR
 fi
-if [ -d out ]; then
-  rm -rf out
+if [ -f out/boot.img ]; then
+  rm -rf out/boot.img
+fi
+if [ -f out/ramdisk.img ]; then
+  rm -rf out/ramdisk.img
 fi
 if [ ! -d system ]; then
   mkdir system
@@ -44,33 +44,34 @@ fi
 if [ ! -d ramdisk/debug_ramdisk ]; then
   mkdir -p ramdisk/debug_ramdisk
 fi
-mkdir -p out
+if [ ! -d out ]; then
+  mkdir -p out
+fi
 mkdir -p $TEMP_MODULES_PATH
 mkdir -p $PRIVATE_MODULE_DIR
 echo "Prepare temp dirs done."
 echo "==========================================="
-modules_array=($(find $KERNEL_DRIVERS_PATH -type f -name *.ko))
-for MODULE in "${modules_array[@]}"
+echo -e "\033[33mRead modules list from $PRIVATE_LOAD_FILE\033[0m"
+echo -e "\033[33mUse DTS as $DTB_PATH\033[0m"
+echo "==========================================="
+modules_ramdisk_array=($(cat $PRIVATE_LOAD_FILE))
+for MODULE in "${modules_ramdisk_array[@]}"
 do
   echo "Copying $MODULE..."
-  cp $MODULE $TEMP_MODULES_PATH/
-  cp $MODULE $PRIVATE_MODULE_DIR/
+  module_file=($(find $KERNEL_DRIVERS_PATH -name $MODULE))
+  cp $module_file $TEMP_MODULES_PATH/
+  cp $module_file $PRIVATE_MODULE_DIR/
 done
 echo "==========================================="
 echo "Generating depmod..."
 depmod -b temp 0.0
-find $TEMP_MODULES_PATH -type f -name *.ko | xargs basename -a > $TEMP_MODULES_PATH/modules.load
-cp $TEMP_MODULES_PATH/modules.load ./modules_scan_result.load
-echo -e "\033[32mSave modules scan result as ./modules_scan_result.load \033[0m"
+#find $TEMP_MODULES_PATH -type f -name *.ko | xargs basename -a > $TEMP_MODULES_PATH/modules.load
+#cp $TEMP_MODULES_PATH/modules.load ./modules_scan_result.load
+#echo -e "\033[32mSave modules scan result as ./modules_scan_result.load \033[0m"
 echo "Generate depmod done."
 
 cp $TEMP_MODULES_PATH/modules.alias $PRIVATE_MODULE_DIR/
-if [ ! -f $PRIVATE_LOAD_FILE ]; then
-  cp $TEMP_MODULES_PATH/modules.load $PRIVATE_MODULE_DIR/
-else
-  echo -e "\033[32mUse specific $PRIVATE_LOAD_FILE as modules.load \033[0m"
-  cp $PRIVATE_LOAD_FILE $PRIVATE_MODULE_DIR/modules.load
-fi
+cp $PRIVATE_LOAD_FILE $PRIVATE_MODULE_DIR/modules.load
 cp $TEMP_MODULES_PATH/modules.dep $PRIVATE_MODULE_DIR/
 cp $TEMP_MODULES_PATH/modules.softdep $PRIVATE_MODULE_DIR/
 
